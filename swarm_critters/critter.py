35893322d953acd9f8958da44fb5ac4576897003
hughie <=>
2025-04-29 12:02:55 +0100

    I think stigmergy really works now! but we'll need to test it

--- swarm_critters/critter.py
+++ swarm_critters/critter.py
@@ -9,30 +9,30 @@ class Critter(FieldAgent):
     type = "critter"
     confidence_multiplier = 1.2
     searching_turn_angle = 20
-    
+
     SEARCHING = 's'
     HOMING = 'h'
 
     def __init__(self, model, nest, sight_range=5):
         super().__init__(model)
-        
+
         self.sight_range = max(sight_range, 1)
         self.vision = []
         self.move_dir = vector2.rand()
         self.nest = nest
         self.state = Critter.SEARCHING
-        
+
         # Hugo's algorithm
         self.confidence = 0
         self.clock = 0
 
     def step(self):
         super().step()
-        
+
         # fill vision
         self.vision.clear()
         self.vision.extend(self.field_neighbors(self.sight_range))
-    
+
         # state behaviour
         if self.state == Critter.HOMING:
             self.homing()
@@ -41,20 +41,20 @@ class Critter(FieldAgent):
         else:
             # if we've somehow entered a bad state, return to the searching state
             self.state = Critter.SEARCHING
-                
+
         # normalize, and turning noise, and move
         self.move_dir = random_turn(vector2.normalized(self.move_dir))
         self.move(*self.move_dir)
 
     # critter wanders along a straight line, searching for flowers
-    def searching(self):                  
+    def searching(self):
         other_flower = None # the closest flower
         other_critters = [] # all neighboring critters
-        
+
         for other in self.vision:
             if other.type != "flower":
                 continue
-            
+
             if self.is_touching(other):
                 other.take_nectar()
                 self.clock = 0
@@ -65,110 +65,119 @@ class Critter(FieldAgent):
 
         # base
         if self.confidence <= 0:
-            self.move_dir = random_turn(self.move_dir, Critter.searching_turn_angle)           
+            self.move_dir = random_turn(self.move_dir, Critter.searching_turn_angle)
         else:
-            self.confidence = max(0, self.confidence - 1)   
+            self.confidence = max(0, self.confidence - 1)
 
     # Critter will move towards the nest, and deposit nectar
     # then will pick a random direction and enter the wandering state
     def homing(self):
         if self.is_touching(self.nest):
-            self.nest.deposit_nectar(self, time=self.clock)            
+            # TODO
             self.state = Critter.SEARCHING
-            self.move_dir = np.multiply(self.move_dir, -1)
-            self.confidence = int(self.clock * Critter.confidence_multiplier)
+            nest_instructions = self.nest.deposit_nectar(time=self.clock, direction=self.move_dir)
+
+            # if no instructions, go back in the direction you came from
+            # same if instructions would take longer than our clock (ie food is further away than the source we just visited)
+            if nest_instructions == None or nest_instructions[0] > self.clock:
+                self.confidence = int(self.clock * Critter.confidence_multiplier)
+                self.move_dir = np.multiply(self.move_dir, -1)
+            else:
+                self.confidence = int(nest_instructions[0] * Critter.confidence_multiplier)
+                self.move_dir = nest_instructions[1]          
+
         else:
             self.move_towards(self.nest.pos)
             self.clock += 1
-        
+
     def is_confident(self):
         return 0 < self.confidence
-    
-    # Incorporates separation steer of boid like behaviour into move_dir 
+
+    # Incorporates separation steer of boid like behaviour into move_dir
     # defined as the sum of the negative relative distances to all nearby critters
     def separation(self, others, weight=1):
         if 1 > len(others):
             return
-        
+
         steer = np.zeros(2)
         for other in others:
             steer = np.add(steer, np.negative(self.relative_position(other.pos)))
-        
+
         if weight != 1:
             steer = np.multiply(steer, weight)
-        
+
         self.move_dir = np.add(steer, self.move_dir)
-    
+
     # Incorporates alignment
     # defined as the mean of the movement directions of all nearby critters
     def alignment(self, others, weight=1):
         if 1 > len(others):
             return
-        
+
         directions = np.array([
             other.move_dir
             for other
             in others
         ])
-        
+
         if 0 >= len(directions):
             return
-        
+
         steer = np.mean(directions, 0)
 
         if weight != 1:
             steer = np.multiply(steer, weight)
 
         self.move_dir = np.add(steer, self.move_dir)
-    
+
     # Incorporates cohesion
     # defined as the mean position of all nearby critters
     def cohesion(self, others, weight=1):
         if 1 > len(others):
-            return        
+            return
 
         positions = np.array([
             self.relative_position(other.pos)
             for other
             in others
         ])
-        
+
         if 0 >= len(positions):
             return
-        
+
         steer = np.mean(positions, 0)
 
         if weight != 1:
             steer = np.multiply(steer, weight)
-        
+
         self.move_dir = np.add(steer, self.move_dir)
 
 
 class SocialCritter(Critter):
-    
+
     def __init__(self, model, nest, sight_range=5):
         super().__init__(model, nest, sight_range)
-    
+
     def searching(self):
         super().searching()
-        
+
         # If we've started homing, return early
         if self.state == Critter.HOMING:
             return
-        
+
         # If we're confident then we don't care what anyone else is doing
         if self.is_confident():
             return
-        
+
         to_retrace = []
         to_follow = []
         to_avoid = []
-        
+
         # Otherwise see what the others are doing
         for other in self.vision:
             if other.type != "critter":
                 continue
-            
+
             # if they're homing, go on the opposite direction to them
             if other.state == Critter.HOMING:
                 to_retrace.append(other)
@@ -176,11 +185,11 @@ class SocialCritter(Critter):
                 to_follow.append(other)
             else:
                 to_avoid.append(other)
-        
+
         # pass it on to boid logic
         self.alignment(to_retrace, -1)
         self.cohesion(to_follow)
-        self.separation(to_avoid)         
+        self.separation(to_avoid)
 
 def random_turn(v, amount=1):
     return vector2.rotated(v, np.random.randint(-amount, amount+1))
--- swarm_critters/nest.py
+++ swarm_critters/nest.py
@@ -1,16 +1,20 @@
 import mesa
+import numpy as np
 
+from collections import deque
 from .field_agent import FieldAgent
 
 class Nest(FieldAgent):
     colour = "firebrick1"
     type = "nest"
     
-    def __init__(self, *args, cycle_length = 365, **kwargs):
+    def __init__(self, *args, cycle_length = 365, pheromone_queue_len=10, **kwargs):
         super().__init__(*args, **kwargs)
         self.nectar = 0
         self.cycle_length = cycle_length
         self.nectar_per_cycle = []
+        self.pheromone_queue = deque()
+        self.pheromone_queue_max_len = pheromone_queue_len
     
     def step(self):
         super().step()
@@ -20,7 +24,16 @@ class Nest(FieldAgent):
             print(self.nectar_per_cycle)
             
     
-    def deposit_nectar(self, critter, time=0):
+    def deposit_nectar(self, time=0, direction=None):
         # Takes nectar deposited by a critter
-        # TODO pheromone queue
         self.nectar += 1
+        
+        # enqueue
+        self.pheromone_queue.append((time, np.multiply(direction, -1)))
+        
+        # dequeue (if full)
+        if len(self.pheromone_queue) < self.pheromone_queue_max_len:
+            return None # ie a signal to just go back in their direction
+        else:
+            return self.pheromone_queue.popleft()
+        
