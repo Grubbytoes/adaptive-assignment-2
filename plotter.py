65785f83a50796b60f7f717a1b18cb7fb358f0fb
Hugo <hugoaaabbott@gmail.com>
2025-05-02 20:18:40 +0100

    Ready for pull

--- plotter.py
+++ plotter.py
@@ -6,8 +6,24 @@ from itertools import product
 sizes = ["small", "medium", "large"]
 types = ["lonely", "social"]
 
-for size, type in product(sizes, types):
+def open_and_load(size, type):
     filename = f"{size}.{type}.json"
     data = json.load(open("results/"+filename, 'r'))
+    return data
+
+for s in sizes:
+    lonely_data = open_and_load(s, "lonely")
+    social_data = open_and_load(s, "social")
     
-    # plotting here
+    # seasons
+    plt.grid(axis="y")
+    for i in range(1, 4):
+        plt.axvline(i*12, c='tomato', ls="dashed", linewidth="1")
+        
+    plt.plot(lonely_data["nectar/cycle/critter"], label="lonely")
+    plt.plot(social_data["nectar/cycle/critter"], label="social", c='hotpink')
+    plt.ylabel("nectar per cycle per critter")
+    plt.xlabel("cycle")
+    
+    plt.savefig(f"results/{s}.png")
+    plt.close()
